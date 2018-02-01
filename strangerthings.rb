5ae4f9cfad5b1341f2ebecfcf6d8509d476d6591
DomoniqueS <stewartdomonique01@gmail.com>
2018-02-01 14:13:10 -0800

    Update strangerthings.rb

--- strangerthings.rb
+++ strangerthings.rb
@@ -1,18 +1,118 @@
 use_bpm 160
 
 play :c4
-sleep 1
+sleep 0.5
 play :e4
-sleep 1
+sleep 0.5
 play :g4
-sleep 1
+sleep 0.5
 play :b4
-sleep 1
+sleep 0.5
+
 play :b4
-sleep 1
+sleep 0.5
 play :g4
-sleep 1
+sleep 0.5
 play :e4
-sleep 1
+sleep 0.5
 play :c4
-sleep 1
+sleep 0.5
+
+play :c4
+sleep 0.5
+play :e4
+sleep 0.5
+play :g4
+sleep 0.5
+play :b4
+sleep 0.5
+
+play :b4
+sleep 0.5
+play :g4
+sleep 0.5
+play :e4
+sleep 0.5
+play :c4
+sleep 0.5
+
+play :c4
+sleep 0.5
+play :e4
+sleep 0.5
+play :g4
+sleep 0.5
+play :b4
+sleep 0.5
+
+play :b4
+sleep 0.5
+play :g4
+sleep 0.5
+play :e4
+sleep 0.5
+play :c4
+sleep 0.5
+
+play :c4
+sleep 0.5
+play :e4
+sleep 0.5
+play :g4
+sleep 0.5
+play :b4
+sleep 0.5
+
+play :b4
+sleep 0.5
+play :g4
+sleep 0.5
+play :e4
+sleep 0.5
+play :c4
+sleep 0.5
+
+play :c4
+sleep 0.5
+play :e4
+sleep 0.5
+play :g4
+sleep 0.5
+play :b4
+sleep 0.5
+
+play :b4
+sleep 0.5
+play :g4
+sleep 0.5
+play :e4
+sleep 0.5
+play :c4
+sleep 0.5
+
+play :c4
+sleep 0.5
+play :e4
+sleep 0.5
+play :g4
+sleep 0.5
+play :b4
+sleep 0.5
+
+play :b4
+sleep 0.5
+play :g4
+sleep 0.5
+play :e4
+sleep 0.5
+play :c4
+sleep 0.5
+
+play :c4
+sleep 0.5
+play :e4
+sleep 0.5
+play :g4
+sleep 0.5
+play :b4
+sleep 0.5
