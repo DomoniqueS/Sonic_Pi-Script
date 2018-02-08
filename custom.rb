95959eac502527a0dbcd01febd3e9d9555824eba
DomoniqueS <stewartdomonique01@gmail.com>
2018-02-08 14:06:04 -0800

    Update custom.rb

--- custom.rb
+++ custom.rb
@@ -1,15 +1,22 @@
-play :E5
-sleep 0.2
-play :E3
-sleep 0.2
-play :B2
-sleep 0.2
-play :B2
-sleep 0.2
-play :E3
-sleep 0.2
-play :E5
-sleep 0.2
-play :E5
-sleep 0.2
+use_bpm 60
+live_loop:soo do
+end
+use_synth :dtri
+x=21
+2.times do
+  play x
+  sleep 1
+  x=x+1
+end
+
+use_bpm 180
+live_loop:soo do
+end
+use_synth :piano
+y=21
+5.times do
+  play y
+  sleep 1
+  y=y+1
+end
 
