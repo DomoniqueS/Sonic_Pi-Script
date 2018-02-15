77b7186c5715e48aacbc3367bcd718f59d2035bf
DomoniqueS <stewartdomonique01@gmail.com>
2018-02-15 14:33:51 -0800

    Create array.rb

---
+++ array.rb
@@ -0,0 +1,13 @@
+use_bpm 120
+use_synth :piano
+use_synth_defaults sustain: 0.25
+slump = [1.5,0.5,0.75,0.25,0.5,0.5,2,2]
+thing= [ :e3,:e3,:g3,:e3,:r,:d3,:c3,:b2]
+live_loop :white_stripes do
+  index = 0
+  8.times do
+    play thing[index]
+    sleep slump[index]
+    index=index+1
+  end
+end
