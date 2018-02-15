c1b9e9bff92beccbb0ea46333413d129e983a7c0
DomoniqueS <stewartdomonique01@gmail.com>
2018-02-15 14:25:06 -0800

    Update aintgotyou.rb

--- aintgotyou.rb
+++ aintgotyou.rb
@@ -1,21 +1,28 @@
+use_bpm 118
 use_synth:piano
 play:c5
-sleep 0.6
+sleep 6
 play:a4
-sleep 0.6
+sleep 6
 play:g4
-sleep 0.6
+sleep 6
 play:fs4
-sleep 0.4
+sleep 4
 play:g4
-sleep 0.1
+sleep 1
 play:a4
-sleep 0.1
+sleep 1
 play:b4
-sleep 0.6
+sleep 5
 play:a4
-sleep 0.4
+sleep 4
 play:a4
-sleep 0.1
-play:ab4
-sleep 0.1
+sleep 1
+play:fs4
+sleep 1
+
+
+
+
+
+
