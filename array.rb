6141ce49ef8c1c9ed7f496d11fa1509c559e90f9
DomoniqueS <stewartdomonique01@gmail.com>
2018-02-23 14:22:52 -0800

    Update array.rb

--- array.rb
+++ array.rb
@@ -1,23 +1,19 @@
-# Welcome to Sonic Pi v2.11
+# Welcome to Sonic Pi v2.11.1
 use_bpm 118
-use_synth:piano
-play:c5
-sleep 6
-play:a4
-sleep 6
-play:g4
-sleep 6
-play:fs4
-sleep 4
-play:g4
-sleep 1
-play:a4
-sleep 1
-play:b4
-sleep 5
-play:a4
-sleep 4
-play:a4
-sleep 1
-play:fs4
-sleep 1
+use_synth :piano
+use_synth_defaults sustain: 0.15
+slow= [5.5,5.5,5.5,4,1.5,1.5,5.5,4.5,5.5,1]
+IA= [ :c5,:a4,:g4,:fs4,:g4,:a4,:b4,:a4,:a4,:ab4]
+live_loop :white_stripes do
+  index = 0
+  5.times do
+    sample :guit_harmonics, rate: (rrand 0.125, 1.5)
+    sleep rrand(0.2, 2)
+  end
+  10.times do
+    play IA[index]
+    sleep slow[index]
+    index=index+1
+    
+  end
+end
