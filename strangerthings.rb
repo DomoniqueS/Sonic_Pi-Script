6cf96dfbaf5d726d11edc67bd31f6f589d3003ce
DomoniqueS <stewartdomonique01@gmail.com>
2018-02-23 14:04:49 -0800

    Update strangerthings.rb

--- strangerthings.rb
+++ strangerthings.rb
@@ -1,120 +1,22 @@
+# Welcome to Sonic Pi v2.11.1
 use_bpm 160
-live_loop:foo do
-  with_fx:reverb do
-
-play :c4
-sleep 0.5
-play :e4
-sleep 0.5
-play :g4
-sleep 0.5
-play :b4
-sleep 0.5
-
-play :b4
-sleep 0.5
-play :g4
-sleep 0.5
-play :e4
-sleep 0.5
-play :c4
-sleep 0.5
-
-play :c4
-sleep 0.5
-play :e4
-sleep 0.5
-play :g4
-sleep 0.5
-play :b4
-sleep 0.5
-
-play :b4
-sleep 0.5
-play :g4
-sleep 0.5
-play :e4
-sleep 0.5
-play :c4
-sleep 0.5
-
-play :c4
-sleep 0.5
-play :e4
-sleep 0.5
-play :g4
-sleep 0.5
-play :b4
-sleep 0.5
-
-play :b4
-sleep 0.5
-play :g4
-sleep 0.5
-play :e4
-sleep 0.5
-play :c4
-sleep 0.5
-
-play :c4
-sleep 0.5
-play :e4
-sleep 0.5
-play :g4
-sleep 0.5
-play :b4
-sleep 0.5
-
-play :b4
-sleep 0.5
-play :g4
-sleep 0.5
-play :e4
-sleep 0.5
-play :c4
-sleep 0.5
-
-play :c4
-sleep 0.5
-play :e4
-sleep 0.5
-play :g4
-sleep 0.5
-play :b4
-sleep 0.5
-
-play :b4
-sleep 0.5
-play :g4
-sleep 0.5
-play :e4
-sleep 0.5
-play :c4
-sleep 0.5
-
-play :c4
-sleep 0.5
-play :e4
-sleep 0.5
-play :g4
-sleep 0.5
-play :b4
-sleep 0.5
-
-play :b4
-sleep 0.5
-play :g4
-sleep 0.5
-play :e4
-sleep 0.5
-play :c4
-sleep 0.5
-
-play :c4
-sleep 0.5
-play :e4
-sleep 0.5
-play :g4
-sleep 0.5
-play :b4
-sleep 0.5
+use_synth :saw
+strange= [ :c2,:e2,:g2,:b2,:c3,:b2,:g2,:e2]
+live_loop :main_theme do
+  with_fx :distortion do
+    # First set of eighth notes
+    index = 0
+    4.times do
+      sample :ambi_swoosh, rate: (rrand 0.125, 1.5)
+      sleep rrand(0.2, 2)
+    end
+    8.times do
+      play strange[index]
+      sleep 0.5
+      index=index+1
+      
+      # Second set of eigth notes
+      
+    end
+  end
+end
